1226cae25430b593f04c118bd0998c71b1b966b9
RaeHoon Baek <bmp117@naver.com>
2023-03-03 19:28:00 +0900

    네이버 boostcource <iOS 앱 프로그래밍> / 5. 영화정보 애플리케이션 / 1. 얼럿과 액션시트 / 2) 얼럿과 액션시트 보여주기

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func showAlertController(style: UIAlertController.Style) {
+        let alertController: UIAlertController = UIAlertController(title: "Title", message: "Message", preferredStyle: style)
+        let okAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action: UIAlertAction) in
+            print("OK pressed")
+        })
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        let handler: (UIAlertAction) -> Void
+        handler = { (action: UIAlertAction) in
+            print("action pressed \(action.title ?? "")")
+        }
+        
+        let someAction: UIAlertAction = UIAlertAction(title: "Some", style: UIAlertAction.Style.destructive, handler: handler)
+        let anotherAction: UIAlertAction = UIAlertAction(title: "Another", style: UIAlertAction.Style.default, handler: handler)
+        
+        alertController.addAction(someAction)
+        alertController.addAction(anotherAction)
+        
+        alertController.addTextField { (field: UITextField) in
+            field.placeholder = "플레이스 홀더"
+            field.textColor = UIColor.red
+        }
+        
+        
+        self.present(alertController, animated: true, completion: {
+            print("Alert controller shown")
+        })
+    }
 
+    //MARK: @IBActions
+    @IBAction func touchUpShowAlertButton(_ sender: UIButton) {
+        self.showAlertController(style: UIAlertController.Style.alert)
+    }
+    
+    @IBAction func touchUpShowActionSheetButton(_ sender: UIButton) {
+        self.showAlertController(style: UIAlertController.Style.actionSheet)
+    }
 
 }
 
